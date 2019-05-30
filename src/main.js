ce40aeaf24941640f9df3591251a9a07e8d3c80e
SupinePandora43 <xbox.account@bk.ru>
2019-05-30 22:00:28 +0500

    electron

--- src/main.js
+++ src/main.js
@@ -10,22 +10,12 @@ let mainWindow;
 const ipcMain = electron.ipcMain;
 function createWindow() {
 	mainWindow = new BrowserWindow({ width: 800, height: 600 });
-	if (process.env.NODE_ENV == "development") {
-		/*async function start_react_scripts() {
-			const spawn = require('react-dev-utils/crossSpawn');
-			require("react-scripts/scripts/start")
-		}
-		start_react_scripts();*/
-		mainWindow.loadURL("http://localhost:3000");
-	} else {
-		const startUrl = url.format({
-			pathname: path.join(__dirname, '/../build/index.html'),
-			protocol: 'file:',
-			slashes: true
-		});
-		mainWindow.loadURL(startUrl);
-	}
-	//mainWindow.webContents.openDevTools();
+	const startUrl = process.env.NODE_ENV == "development" ? "http://localhost:3000" : url.format({
+		pathname: path.join(__dirname, '/../build/index.html'),
+		protocol: 'file:',
+		slashes: true
+	});
+	mainWindow.loadURL(startUrl);
 
 	mainWindow.on('closed', function () {
 		mainWindow = null;
